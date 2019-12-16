afdb918b20c1e37cfc4e4ed6980ac7d2638c6ccd
Thorsten Lorenz <thlorenz@gmx.de>
2019-12-16 12:34:43 -0500

    types: satisfyAny supporting looser specification type

--- src/jest-spok.ts
+++ src/jest-spok.ts
@@ -1,7 +1,7 @@
 import spok, { Specifications } from 'spok'
-import ocat = require('ocat')
 import deepEqual from 'deep-equal'
 import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils'
+import ocat = require('ocat')
 
 function prettyFailed(failed: string[]) {
   return failed.map((x) => '  ' + printExpected(x)).join('\n')
@@ -55,19 +55,37 @@ function toSatisfy<T extends object>(
   received: T,
   expected?: Specifications<T>
 ) {
+  return satisfy(received, expected)
+}
+
+function toSatisfyAny<P extends object, T extends object>(
+  received: T,
+  expected?: P
+) {
+  return satisfy(received, expected, true)
+}
+
+function satisfy<P extends object, T extends object>(
+  received: T,
+  expected?: P,
+  any: boolean = false
+) {
   if (expected == null) {
     return { pass: false, message: renderMessage(received) }
   }
 
   const assert = new Assert()
-  spok(assert, received, expected)
+  if (any) {
+    spok(assert, received, expected)
+  } else {
+    spok.any(assert, received, expected)
+  }
   const pass = assert.failed.length === 0
 
   if (pass) return { pass: true, message: () => 'passed' }
   return { pass: false, message: failMessage(received, expected, assert) }
 }
-
-export default { toSatisfy }
+export default { toSatisfy, toSatisfyAny }
 export const ocatOpts = ocat.opts
 export { spok }
 
@@ -80,7 +98,18 @@ declare global {
        * expect({ foo: 1, bar: {} }).toSatisfy({ foo: spok.ge(2), bar: spok.string })
        */
       toSatisfy<T>(expected?: Specifications<T>): JestMatchers<T>
+
+      /**
+       * Asserts that a value matches the provided spok specifications using
+       * `spok.any` and thus allowing a less strict specification type.
+       *
+       * Use ONLY when you cannot adjust the types, so plain `toSatisfy` works.
+       */
+      toSatisfyAny<P extends object, T extends object>(
+        expected?: P
+      ): JestMatchers<T>
     }
+
     interface Matchers<R, T> {
       /**
        * Asserts that a value matches the provided spok specifications.
@@ -88,6 +117,14 @@ declare global {
        * expect({ foo: 1, bar: {} }).toSatisfy({ foo: spok.ge(2), bar: spok.string })
        */
       toSatisfy(expected?: Specifications<T>): R
+
+      /**
+       * Asserts that a value matches the provided spok specifications using
+       * `spok.any` and thus allowing a less strict specification type.
+       *
+       * Use ONLY when you cannot adjust the types, so plain `toSatisfy` works.
+       */
+      toSatisfyAny<P extends object, T extends object>(expected?: P): R
     }
   }
 }
